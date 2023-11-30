c466d9c489369bb1ea5735a196d5b9538be17782
Shahidullah Muffakir <shahidmuffakir@gmail.com>
2023-12-01 00:31:13 +0530

    initial commit

---
+++ clothingData.js
@@ -0,0 +1,132 @@
+// clothingData.js
+const clothingItems = [
+  {
+    name: "Traditional Afghan Dress",
+    image: "./images/img7.jpeg",
+    description:
+      "A beautiful traditional Afghan dress with intricate embroidery.",
+  },
+  {
+    name: "Afghan Turban",
+    image: "./images/img15.jpeg",
+    description: "An elegant Afghan turban to complete your traditional look.",
+  },
+
+  {
+    name: "Elegant Afghan Shawl",
+    image: "./images/img16.jpeg",
+    description:
+      "Wrap yourself in the beauty of this elegant Afghan shawl with intricate patterns.",
+  },
+  {
+    name: "Traditional Pashtun Cap",
+    image: "./images/img17.jpeg",
+    description:
+      "A traditional Pashtun cap that adds a touch of cultural authenticity to your attire.",
+  },
+  {
+    name: "Silk Embroidered Afghan Robe",
+    image: "./images/img18.jpeg",
+    description:
+      "Indulge in the luxury of a silk embroidered Afghan robe, perfect for special occasions.",
+  },
+  {
+    name: "Handwoven Kuchi Dress",
+    image: "./images/img19.jpeg",
+    description:
+      "Experience the craftsmanship of a handwoven Kuchi dress, a blend of tradition and style.",
+  },
+  {
+    name: "Afghan Tribal Jewelry Set",
+    image: "./images/img20.jpeg",
+    description:
+      "Adorn yourself with this stunning Afghan tribal jewelry set, a masterpiece of cultural artistry.",
+  },
+  {
+    name: "Casual Afghan Tunic",
+    image: "./images/img1.jpeg",
+    description:
+      "Stay comfortable and stylish in this casual Afghan tunic, perfect for everyday wear.",
+  },
+  {
+    name: "Colorful Kandahari Scarf",
+    image: "./images/img2.jpeg",
+    description:
+      "Add a pop of color to your outfit with this vibrant Kandahari scarf, a true fashion statement.",
+  },
+  {
+    name: "Afghan Bridal Lehenga",
+    image: "./images/img3.jpeg",
+    description:
+      "Celebrate your special day in an exquisite Afghan bridal lehenga, symbolizing grace and elegance.",
+  },
+  {
+    name: "Embroidered Balochi Dress",
+    image: "./images/img4.jpeg",
+    description:
+      "Step into the world of tradition with this beautifully embroidered Balochi dress.",
+  },
+  {
+    name: "Afghan Nomad Poncho",
+    image: "./images/img5.jpeg",
+    description:
+      "Embrace the nomadic spirit with this stylish Afghan nomad poncho, a blend of comfort and culture.",
+  },
+  {
+    name: "Classic Pashtun Chapan",
+    image: "./images/img6.jpeg",
+    description:
+      "Make a timeless statement with the classic Pashtun chapan, a symbol of Afghan heritage.",
+  },
+  {
+    name: "Embroidered Pashmina Shawl",
+    image: "./images/img7.jpeg",
+    description:
+      "Wrap yourself in warmth and beauty with this intricately embroidered Pashmina shawl.",
+  },
+  {
+    name: "Afghan Kite Runner Pants",
+    image: "./images/img8.jpeg",
+    description:
+      "Experience comfort and style with the Afghan kite runner pants, inspired by cultural motifs.",
+  },
+  {
+    name: "Handcrafted Afghan Sandals",
+    image: "./images/img9.jpeg",
+    description:
+      "Complete your look with these handcrafted Afghan sandals, a perfect fusion of tradition and fashion.",
+  },
+  {
+    name: "Afghanistan Flag-inspired Scarf",
+    image: "./images/img30.jpeg",
+    description:
+      "Show your patriotic spirit with this Afghanistan flag-inspired scarf, a unique accessory.",
+  },
+  {
+    name: "Vintage Afghan Vest",
+    image: "./images/img10.jpeg",
+    description:
+      "Add a touch of vintage charm to your outfit with this beautifully crafted Afghan vest.",
+  },
+  {
+    name: "Afghan Street Style Jacket",
+    image: "./images/img11.jpeg",
+    description:
+      "Stay trendy and warm with this Afghan street style jacket, perfect for urban adventures.",
+  },
+  {
+    name: "Nomadic Embroidered Hat",
+    image: "./images/img12.jpeg",
+    description:
+      "Top off your look with this nomadic embroidered hat, a unique blend of tradition and flair.",
+  },
+  {
+    name: "Afghanistan Pride T-shirt",
+    image: "./images/13.jpeg",
+    description:
+      "Wear your pride with this Afghanistan-themed T-shirt, featuring cultural motifs and symbols.",
+  },
+];
+
+// Export the array
+export default clothingItems;
